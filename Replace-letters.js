e6003ebe95dc5547105a30d4feaf7c04c1c3de00
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-31 19:17:49 -0700

    Update Replace-letters.js

--- Replace-letters.js
+++ Replace-letters.js
@@ -22,3 +22,13 @@ Note: You work with lowercase letters only.
 
 
 // Solution
+
+function replaceLetters(word) {
+  const charMap = {
+    'a': 'z', 'b': 'e', 'c': 'e', 'd': 'e', 'e': 'd', 'f': 'i', 'g': 'i', 'h': 'i', 'i': 'h',
+    'j': 'o', 'k': 'o', 'l': 'o', 'm': 'o', 'n': 'o', 'o': 'n', 'p': 'u', 'q': 'u', 'r': 'u',
+    's': 'u', 't': 'u', 'u': 't', 'v': 'a', 'w': 'a', 'x': 'a', 'y': 'a', 'z': 'a'
+  };
+  
+  return word.split('').map(e => charMap[e]).join('');
+}
